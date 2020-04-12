660a8fb19f2c88d8557f79163678816caea8a856
Richard Young <richard.young2@ibm.com>
2020-04-12 13:05:05 +0200

    Add card buttons

--- public/components/game-card/game-card.js
+++ public/components/game-card/game-card.js
@@ -1,22 +1,34 @@
-const template = obj => `
+const template = (props) => `
     <style>
-        .card {
-            background-color: white;
-            width: 100px;
-            height: 150px;
-            border-radius: 5px;
-            box-shadow: 2px 2px 10px 0px rgba(0,0,0,0.75);
-            margin-left: -50px;
-        }
+      :host {
+          display: block;
+      }
+      .card {
+        display: flex;
+        flex-direction: column;
+        box-sizing: border-box;
+        background-color: white;
+        color: ${props.colour};
+        width: 100px;
+        height: 150px;
+        border-radius: 5px;
+        box-shadow: 2px 2px 10px 0px rgba(0,0,0,0.75);
+        padding: 0.5em;
+        
+        margin-top: -100px;
+        margin-left: -50px;
+
+      }
     </style>
 
     <div class="card">
-    
-    </div>`
+        <div>${props.value}</div>
+        <div>${props.icon}</div>
+    </div>`;
 
 customElements.define('game-card', class GameCard extends HTMLElement {
   static get observedAttributes() {
-    return ['name'];
+    return ['colour', 'value', 'icon', 'suite'];
   }
 
   constructor() {
